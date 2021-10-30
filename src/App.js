541a146af5e83f88592d9797c2d5f0ed25bf7829
DannySo <wg05095@hanmail.net>
2021-10-31 04:30:53 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
   let [글제목, 글제목변경] = useState(orgSubject);
   let [따봉, 따봉변경] = useState(0);
   let [flag, changeFlag] = useState(true);
-  
+
+  let [modal,modal변경] = useState(false);
+  let [누른제목,누른제목변경] = useState(0);
+  let [입력값,입력값변경] = useState('');
+
   function 제목바꾸기() {
     
     if(flag){
@@ -23,38 +27,49 @@ function App() {
     }
     
   }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
       <button onClick={ 제목바꾸기 }>버튼</button>
-      <div className="list1">
-        <h3>{ 글제목[0] } <span onClick={ ()=>{따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
-        <p>10월 31일 발생</p>
-        <hr/>
-      </div>
-      <div className="list1">
-        <h3>{ 글제목[1] }</h3>
-        <p>10월 31일 발생</p>
-        <hr/>
-      </div>
-      <div className="list1">
-        <h3>{ 글제목[2] }</h3>
-        <p>10월 31일 발생</p>
-        <hr/>
+
+      {
+        글제목.map(function(글,i){
+          return (
+            <div className="list1" key={i}>
+              <h3 onClick={ ()=>{ 누른제목변경(i) } }>{ 글 } <span onClick={ ()=>{따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
+              <p>10월 31일 발생</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
+      <div className="publish">
+        <input onChange={ (e)=>{입력값변경(e.target.value) }}/>
+        <button onClick={ ()=>{ 
+          var arrayCopy = [...글제목];
+          arrayCopy.unshift(입력값)
+          글제목변경(arrayCopy);
+          } }>저장</button>
       </div>
 
-      <Modal />
-
+      <button onClick={ () => { modal변경(!modal) } }>모달</button>
+      {
+        modal === true 
+        ? <Modal 글제목={글제목} 누른제목={누른제목}/> 
+        : null
+      }
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{ props.글제목[props.누른제목] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
